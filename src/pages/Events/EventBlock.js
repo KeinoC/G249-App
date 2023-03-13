e283920e4dda60c6a0179d49c240a0ac913ea26d
Keino Chichester <keino.chichester@gmail.com>
2023-03-13 01:30:29 -0400

    update

--- src/pages/Events/EventBlock.js
+++ src/pages/Events/EventBlock.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./EventBlock.css";
 
 
@@ -13,11 +13,29 @@ export default function EventBlock ({
     eventNotes,
     eventPrice
 }){
+
+
     
+function handleDelete(e) {
+
+    let url = `https://api.sheety.co/3132ef9a7c5a88398c1881749e1528c7/testDataForG249Api/events/${id}`;
+    fetch(url, {
+      method: 'DELETE',
+    })
+    .then((response) => response.json())
+    .then(() => {
+      console.log('Object deleted');
+    });
+
+     const element = document.getElementById(id)
+     element.classList.add("hidden")
+     element.classList.remove("event-block")
+
+}
 
 
 return (
-    <div className = "event-block">
+    <div id = {id} className = "event-block" >
 
         <div className = "date-block e-block"> {eventDate}</div>
         <div className = "price-block e-block"> {eventPrice}</div>
@@ -40,8 +58,8 @@ return (
             
             <div className = "delete-block">
                 {/* <img src= './public/trash.svg' className = "trash-icon"/> */}
-                <svg value = {id} className = "trash-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <svg value={id} onClick={handleDelete} className = "trash-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+                <path  d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                 </svg>
             </div>
