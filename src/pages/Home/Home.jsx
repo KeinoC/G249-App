4486fabc7b30f3a179342625d1d47d527fe52054
Keino Chichester <keino.chichester@gmail.com>
2023-03-18 08:35:50 -0400

    Saturday night insomnia. Added temp nav bars to all pages, added all routes. started on abailability page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Home.css"
 import Nav from "../Nav/Nav"
+import {Link} from 'react-router-dom'
 
 export default function Home() {
     return (
--- src/pages/Nav/Nav.jsx
+++ src/pages/Nav/Nav.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Link} from "react-router-dom"
+import "../Home/Home.css"
+import './Nav.css'
 
 export default function Nav() {
     return (
     <nav className = "navbar">
-        <span className = "nav-item">Home</span>
-        <span className = "nav-item">Availability</span>
-        <span className = "nav-item">Drop By</span>
-        <span className = "nav-item">About Us</span>
+        <Link to="/" className = "nav-item">
+            <span >Home</span>
+        </Link>
+
+        <Link to="/availability" className = "nav-item">
+            <span >Availability</span>
+        </Link>
+
+        <Link to ="/tour" className = "nav-item">
+            <span >Drop By</span>
+        </Link>
+        
+        <Link to ="/about" className = "nav-item">
+            <span >About Us</span>
+        </Link>
     </nav>
 )}
